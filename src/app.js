72222351eec41dda68ee901db3b1edc0853454c5
Vasiliy Shubko <flit77@gmail.com>
2017-02-15 00:26:35 +0100

    people exported as resource

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
 import {groupBy} from 'lodash/collection';
+import people from './people';
 
-const people = [{
-  manager: 'Jen',
-  name: 'Bob'
-}, {
-  manager: 'Jen',
-  name: 'Sue'
-}, {
-  manager: 'Bob',
-  name: 'Shirley'
-}, {
-  manager: 'Bob',
-  name: 'Terrence'
-}];
 const managerGroups = groupBy(people, 'manager');
 
 const root = document.querySelector('#root');
