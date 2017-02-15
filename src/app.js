aac209b706d096fc64cb4bdb34343fb65d807e09
Vasiliy Shubko <flit77@gmail.com>
2017-02-15 23:30:27 +0100

    image added to webpack

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import {groupBy} from 'lodash/collection';
 import './style.scss';
 import people from './people';
+import codeURL from './code.png';
 
+const img = document.createElement('img');
 const managerGroups = groupBy(people, 'manager');
 
 const root = document.querySelector('#root');
 root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`;
+
+img.src = codeURL;
+img.style.backgroundColor = "#2B3A42";
+img.style.padding = "20px";
+img.width = 32;
+document.body.appendChild(img);
